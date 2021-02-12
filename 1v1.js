ff0ee9e2bf8a68bd555dc1f711d90742be1e86b0
cyrillebenoit <cyrille.benoit@ymail.com>
2021-02-12 03:34:38 +0100

    Update 1v1.js

--- 1v1.js
+++ 1v1.js
@@ -1,171 +1,171 @@
 ;
 var bingoList = [];
 bingoList[1] = [
-{ name: "11 Delfino Blue Coins"},
-{ name: "15 Delfino Blue Coins"},
-{ name: "5 Linked Blue Coin Pairs"},
-{ name: "3 Shines from Big Bois"},
+{ name: "11 Delfino Blues"},
+{ name: "15 Delfino Blues"},
+{ name: "5 Linked Pairs"},
+{ name: "3 Big Bois"},
 { name: "30 Total Blue Coins"}
 ];
 bingoList[2] = [
-{ name: "Two 100 Coin Shines"},
-{ name: "50f2 OR 150fDelfino"},
-{ name: "❌ Slower Bianco 4 ❌"},
-{ name: "🍭 Slower Ricco 3 🍭"},
+{ name: "Two 100s"},
+{ name: "50 from 2 OR 150 from Delfino"},
+{ name: "Dry Bianco 4"},
+{ name: "Slow Ricco 3 🍭"},
 { name: "Rimato Start"}
 ];
 bingoList[3] = [
-{ name: "Mario x18"},
-{ name: "Mario x20"},
-{ name: "100 Coin Shine from Ricco"},
-{ name: "3 Red Coin Shines"},
-{ name: "1 Hidden Shine in Bianco"}
+{ name: "18 Lives"},
+{ name: "20 Lives"},
+{ name: "100s from Ricco"},
+{ name: "3 Red Coins"},
+{ name: "1 Hidden in Bianco"}
 ];
 bingoList[4] = [
-{ name: "15 Bianco Blue Coins"},
+{ name: "15 Bianco Blues"},
 { name: "15 Ms"},
 { name: "17 Ms"},
-{ name: "Ricco 2 (again)"},
+{ name: "1 Hidden in Ricco"},
 { name: "Spin gang OR River gang"}
 ];
 bingoList[5] = [
-{ name: "3 Fruit Lady Blue Coins"},
-{ name: "5 NPC Blue Coins"},
-{ name: "15 Gelato Blue Coins"},
-{ name: "4 Shines from Sirena"},
-{ name: "3 A cappella Shines"}
+{ name: "3 Happy Ladies"},
+{ name: "5 NPC Blues"},
+{ name: "15 Gelato Blues"},
+{ name: "4 from Sirena"},
+{ name: "3 Secrets"}
 ];
 bingoList[6] = [
-{ name: "1 Turbo Nozzle Unlock"},
-{ name: "3 Hidden Shines"},
-{ name: "9 Sirena Blue Coins"},
+{ name: "1 Turbo"},
+{ name: "3 Hiddens"},
+{ name: "9 Sirena Blues"},
 { name: "GB's favorite hoverless goal"},
-{ name: "2 Hidden Shines in Gelato"}
+{ name: "2 Hiddens in Gelato"}
 ];
 bingoList[7] = [
 { name: "Bianco 5 OR Ricco 5 OR (Noki 2 AND Ricco 1)"},
-{ name: "6 Shines from Ricco"},
-{ name: "2 Hidden Shines in 1 Level (NO Gelato)"},
-{ name: "Sirena 4R (=7 pipes)"},
-{ name: "100 Coin Shine from Bianco"}
+{ name: "6 from Ricco"},
+{ name: "2 Hiddens in 1 Level (NO Gelato)"},
+{ name: "Sirena 4R (= 7 pipes)"},
+{ name: "Bianco Bills"}
 ];
 bingoList[8] = [
-{ name: "16 Pianta Blue Coins"},
-{ name: "2 Rocket Nozzle Unlocks"},
+{ name: "16 Pianta Blues"},
+{ name: "2 Rockets"},
 { name: "4 Unique Big Bois"},
-{ name: "4 Shines from Pianta"},
-{ name: "100 Coin Shine from Gelato AND lose the game"}
+{ name: "4 from Pianta"},
+{ name: "100s from Gelato AND lose the game"}
 ];
 bingoList[9] = [
-{ name: "5 Enemy Blue Coins"},
-{ name: "4 Red Coin Shines"},
-{ name: "23 Ricco Blue Coins"},
-{ name: "5 Blue Bird Blue Coins"},
-{ name: "16 Pinna Blue Coins"}
+{ name: "5 Enemy Blues"},
+{ name: "4 Red Coins"},
+{ name: "23 Ricco Blues"},
+{ name: "5 Blue Birds"},
+{ name: "16 Pinna Blues"}
 ];
 bingoList[10] = [
-{ name: "4 Shines from Big Bois"},
-{ name: "7 Enemy Blue Coins"},
-{ name: "Caterpillar Shine"},
-{ name: "20 Bianco Blue Coins"},
+{ name: "4 Big Bois"},
+{ name: "7 Enemy Blues"},
+{ name: "Caterpillar"},
+{ name: "20 Bianco Blues"},
 { name: "Beach Money"}
 ];
 bingoList[11] = [
-{ name: "8 Delfino Shines"},
-{ name: "9 Delfino Shines"},
-{ name: "1 Hidden Shine in Sirena"},
-{ name: "4 A cappella Shines"},
-{ name: "7 Linked Blue Coin Pairs"}
+{ name: "8 Delfinos"},
+{ name: "9 Delfinos"},
+{ name: "1 Hidden in Sirena"},
+{ name: "4 Secrets"},
+{ name: "7 Linked Pairs"}
 ];
 bingoList[12] = [
-{ name: "1 Hidden Shine in Pinna"},
-{ name: "7 Pinna Beach Blue Coins OR 4 Fruit Lady Blue Coins"},
-{ name: "20 Blue Coins from 2 Levels"},
-{ name: "4 Hidden Shines"},
-{ name: "5 Shines from Big Bois"}
+{ name: "1 Hidden in Pinna"},
+{ name: "4 Happy Ladies OR 7 Sad Pinna Blues"},
+{ name: "20 from 2"},
+{ name: "4 Hiddens"},
+{ name: "5 Big Bois"}
 ];
 bingoList[13] = [
-{ name: "6 Shines from Pinna"},
-{ name: "Pinna 5 from the back Hoverless"},
-{ name: "Pinna Shadow Mario"},
-{ name: "1 Hidden Shine in Pianta"},
+{ name: "6 from Pinna"},
+{ name: "Hit it from the back hoverless"},
+{ name: "Pinna SM"},
+{ name: "1 Hidden in Pianta"},
 { name: "6 Pipes"},
 { name: "9 Pipes"}
 ];
 bingoList[14] = [
 { name: "Both Bell Shines OR Infinite Wallkicks"},
-{ name: "Lily Pad Timer Glitch"},
+{ name: "Timer Glitch"},
 { name: "Ride Yoshi in 2 Worlds (includes Delfino)"},
 { name: "All Pinna Yellow Button Coins"},
-{ name: "5 Red Coin Shines"}
+{ name: "5 Red Coins"}
 ];
 bingoList[15] = [
-{ name: "12 Noki Blue Coins"},
+{ name: "12 Noki Blues"},
 { name: "Sand Dollars"},
 { name: "monkaS 100s"},
 { name: "100s from a Boss Shine"}
 ];
 bingoList[16] = [
-{ name: "7 Blue Bird Blue Coins"},
-{ name: "1 Butterfly Blue Coin (No Pinna)"},
-{ name: "4 Shines from Gelato"},
-{ name: "16 Noki Blue Coins OR 5 Shines from Gelato"},
-{ name: "3 Rocket Nozzle Unlocks"}
+{ name: "7 Blue Birds"},
+{ name: "1 Butterfly"},
+{ name: "4 from Gelato"},
+{ name: "16 Noki Blues OR 5 from Gelato"},
+{ name: "3 Rockets"}
 ];
 bingoList[17] = [
-{ name: "5 A cappella Shines"},
-{ name: "5f2"},
-{ name: "5 Shines from Bianco"},
-{ name: "5 Hidden Shines"},
-{ name: "4 Shines from Noki"}
+{ name: "5 Secrets"},
+{ name: "5 from 2"},
+{ name: "5 from Bianco"},
+{ name: "5 Hiddens"},
+{ name: "4 from Noki"}
 ];
 bingoList[18] = [
 { name: "Both Gold Birds"},
-{ name: "1fe"},
-{ name: "2f5"},
+{ name: "1 from each"},
+{ name: "2 from 5"},
 { name: "Pinna 2 AND Noki 1"}
 ];
 bingoList[19] = [
-{ name: "6 Red Coin Shines"},
-{ name: "100 Coin Shine from Noki"},
-{ name: "18 Sirena Blue Coins"},
-{ name: "Delfino Rocket Nozzle"},
+{ name: "6 Red Coins"},
+{ name: "100s from Noki"},
+{ name: "18 Sirena Blues"},
+{ name: "Delfino Rocket"},
 { name: "26 Shines"}
 ];
 bingoList[20] = [
 { name: "Remember to take proper care of your teeth!"},
-{ name: "Three 100 Coin Shines"},
-{ name: "6 Hidden Shines"},
+{ name: "Three 100s"},
+{ name: "6 Hiddens"},
 { name: "Not 7 secrets!"}
 ];
 bingoList[21] = [
-{ name: "2 Turbo Nozzle Unlocks"},
+{ name: "2 Turbos"},
 { name: "COUNT TO 12"},
-{ name: "12 Shines from Delfino (With Blue Trades)"},
-{ name: "15 Shines from Delfino (With Blue Trades)"},
-{ name: "80 Total Blue Coins"}
+{ name: "12 Delfinos (w/ trades)"},
+{ name: "15 Delfinos (w/ trades)"},
+{ name: "80 Total Blues"}
 ];
 bingoList[22] = [
-{ name: "3f4"},
-{ name: "4f3"},
-{ name: "2f7"},
+{ name: "3 from 4"},
+{ name: "4 from 3"},
+{ name: "2 from 7"},
 { name: "8 Episodes"},
-{ name: "8f1"}
+{ name: "8 from 1"}
 ];
 bingoList[23] = [
 { name: "Pianta 6 OR Ricco 8"},
-{ name: "1 Shadow Mario Shine"},
-{ name: "Pianta Shadow Mario"},
-{ name: "2 Piantissimo Shines"},
-{ name: "All Sand Bird Blues OR All Eel Blues"}
+{ name: "1 Shadow Mario"},
+{ name: "Pianta SM"},
+{ name: "2 Piantissimos"},
+{ name: "Sand Bird Blues OR Eel Blues"}
 ];
 bingoList[24] = [
-{ name: "40 Total Blue Coins"},
-{ name: "45 Total Blue Coins"},
-{ name: "50 Total Blue Coins"}
+{ name: "40 Total Blues"},
+{ name: "45 Total Blues"},
+{ name: "50 Total Blues"}
 ];
 bingoList[25] = [
-{ name: "60 Total Blue Coins"},
-{ name: "65 Total Blue Coins"},
-{ name: "70 Total Blue Coins"}
+{ name: "60 Total Blues"},
+{ name: "65 Total Blues"},
+{ name: "70 Total Blues"}
 ];
